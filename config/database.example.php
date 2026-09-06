dc772d5a583f929fb50e395cc6242bab454ccf6a
Rwaletto <rwaletto@hotmail.com>
2026-09-06 17:30:10 -0300

    database.example.php

---
+++ config/database.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'mysql.soldadosdearuanda.com.br',
+    'database' => 'soldadosdearua02',
+    'username' => 'soldadosdearua02',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
